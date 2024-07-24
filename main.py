46c47a58ba54a04b2d20120eb5c1eadad3e3d89a
John Boja <crisostomoiboja@gmail.com>
2024-07-24 14:55:22 +0800

    SERVO CONTROLLER

--- main.py
+++ main.py
@@ -25,6 +25,7 @@ while True:
         for handLms in multiRes:
             mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
 
+
             for id, lm in enumerate(handLms.landmark):
                 h, w, c = img.shape
                 cx, cy = int(lm.x * w), int(lm.y * h)
